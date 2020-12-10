8405f754841bd150944c28bb4d90d92472892b46
ezra-fy <rymidaly@gmail.com>
2020-12-10 01:39:30 +0000

    Added Tax Calculator Method to Tax Payer

--- 2010/tax_payer.rb
+++ 2010/tax_payer.rb
@@ -1,3 +1,5 @@
+require_relative 'tax_calculator'
+
 def TaxPayer
   attr_accessor :name, :yearly_salary, :tax_credit
 
@@ -5,6 +7,10 @@ def TaxPayer
     @name = name
     @yearly_salary = yearly_salary.to_f
     @tax_credit = tax_credit.to_f
+  end
+
+  def calculate_total_tax_due
+    new TaxCalculator(@yearly_salary, @tax_credit).calculate_tax_due
   end
 
   def to_string
