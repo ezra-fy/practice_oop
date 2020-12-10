24789dbefb3ca2fe160042898393a84c2b6d5b7f
ezra-fy <rymidaly@gmail.com>
2020-12-10 01:33:22 +0000

    Implemented TaxCalculator

--- 2010/tax_calculator.rb
+++ 2010/tax_calculator.rb
@@ -1,1 +1,19 @@
+def TaxCalculator
+  def initialize(yearly_salary, tax_credit)
+    @yearly_salary = yearly_salary.to_f
+    @tax_credit = tax_credit.to_f
+    @tax_threshold = 34000
+    @base_rate = 0.2
+    @higher_rate = 0.41
+  end
 
+  def calculate_tax_due
+    tax_due = 0
+    taxable_salary = @yearly_salary
+    if @yearly_salary > 34000
+      tax_due = (@yearly_salary - 34000) * @higher_rate
+      taxable_salary -= @yearly_salary
+    end
+    tax_due += (taxable_salary * @base_rate) - @tax_credit
+  end
+end
